0e23401183898868993408dcd924621a85aa81a7
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-11-13 17:38:26 +0530

    checking authentication error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,7 @@
 import NextAuth from "next-auth/next"
-import GoogleProvider from 'next-auth/providers/google'
+import { authOptions } from "@/lib/authOptions"
 
 
-const handler = NextAuth({
-  providers : [
-    GoogleProvider({
-      clientId : process.env.GOOGLE_CLIENT_ID ??"",
-      clientSecret : process.env.GOOGLE_CLIENT_SECRET ??"",
-    })
-  ],
-
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub as string;
-      }
-      return session;
-    },
-    async jwt({ token, account }) {
-      if (account) {
-        token.sub = account.providerAccountId;
-      }
-      return token;
-    },
-  },
-
-
-})
+const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import './globals.css'
 // import { useState } from 'react'
 // import Providers from './components/Providers';
 import React from 'react'
-import { SessionProvider } from 'next-auth/react'
+// import { SessionProvider } from 'next-auth/react'
 import { UserProvider } from '@/context/userContext'
+import SessionWrapper from '@/components/SessionWrapper'
 
 export default function RootLayout({
   children,
@@ -18,17 +19,19 @@ export default function RootLayout({
   // }
 
   return (
-    <html lang="en">
-      <body className="dark">
-        <UserProvider>
-          <SessionProvider>
+    <SessionWrapper>
+      <html lang="en">
+        <body className="dark">
+          <UserProvider>
+            {/* <SessionProvider> */}
             {children}
             {/* <div className={`fixed top-[3%] left-[85%] cursor-pointer ${theme === 'dark' ? 'text-white' : 'text-black'}`} onClick={toggle}>
-              {theme === 'dark' ? 'light' : 'dark'}
-            </div> */}
-          </SessionProvider>
-        </UserProvider>
-      </body>
-    </html>
+                {theme === 'dark' ? 'light' : 'dark'}
+              </div> */}
+            {/* </SessionProvider> */}
+          </UserProvider>
+        </body>
+      </html>
+    </SessionWrapper>
   )
 }
