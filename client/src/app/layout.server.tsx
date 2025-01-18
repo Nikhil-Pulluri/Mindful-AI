ddca94729a445e1f6eb2c43253298325ecc1e4e4
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-01-18 08:26:02 +0530

    structuring the directory

--- client/src/app/layout.server.tsx
+++ client/src/app/layout.server.tsx
@@ -1,8 +1,6 @@
-
-
-import type { Metadata } from 'next';
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Landing',
   description: 'Landing Page here',
-};
+}
