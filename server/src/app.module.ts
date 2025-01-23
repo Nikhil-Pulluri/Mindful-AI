e6ab8a3c6b3ef4fdb4e0b85bf7afe210b39c18ce
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-01-23 21:57:25 +0530

    prisma initialised and chat module generated

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ServiceModule } from './controller/service/service.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ServiceModule, ChatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
