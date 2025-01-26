6eb18acf8839581fd3b05cbe01332937f41b9919
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-01-26 21:34:53 +0530

    pushing unsaved chages

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Get } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,7 +7,6 @@ export class UserService {
 
   constructor(private prisma: PrismaService){}
 
-  @Get()
   async getAllUsers() : Promise<User[]> {
     return this.prisma.user.findMany()
   }
